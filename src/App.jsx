5a7882d64a18cb716b782e8eea4dcedfdcf8109f
najot <otabekyusupov3738@gmail.com>
2023-04-09 05:31:24 +0500

    todotask

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer';
+import Main from './components/Main/Main';
+// import img from './assets/images/man.png';
+function App() {
+ 
+          
+    return (
+        <>
+            <Header />
+           <Main />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './List';
+import "./index.scss";
+
+function index() {
+    return (
+   <>
+   </>
+    );
+}
+
+export default index;
